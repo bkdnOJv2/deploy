0f96f328846c81915d2872a7b9605ef0a3c1f73a
Ngo Van Anh Tuan <tuan.nva@teko.vn>
2023-07-01 13:56:51 +0700

    Update scripts and env

--- bkdnojv2/scripts/initdata.sh
+++ bkdnojv2/scripts/initdata.sh
@@ -2,3 +2,4 @@
 export COMPOSE_EXEC_FLAGS=-T
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@bkdnojv2.com', 'admin')" | bash ./scripts/manage.sh shell
 bash ./scripts/manage.sh loaddata lang_small
+echo "from judger.models import Judge; Judge.objects.create(name='localjudge', auth_key='localkey')" | bash ./scripts/manage.sh shell
